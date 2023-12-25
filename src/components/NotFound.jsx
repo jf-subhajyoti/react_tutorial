2d914ff53bcdd090bc708c6040af6b1a74a8d83c
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-25 20:24:09 +0530

    Navigate component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>
-      
-    </div>
+    // It navigates it to home page when we reach to not found page
+    <Navigate to='/' />
   )
 }
 
